c9252554df9c04cd65741fe3a37771e8066898b2
unisanta <unisanta@santacecilia.br>
2025-04-16 19:44:14 -0300

    estrtura desenvolvida

---
+++ src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { Tarefa } from './tarefa';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'TODOapp';
+  apiURL: string = 'http://localhost:3000';
+
+  constructor(private service: HttpClient) {
+    this.READ_tarefas();
+  }
+
+  arrayDeTarefas: Tarefa[] = [];
+
+  READ_tarefas() {
+    this.service.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
+      resultado => this.arrayDeTarefas=resultado);
+  }
+
+  CREATE_tarefa(descricaoNovaTarefa: string) {
+    var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+    this.service.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); });
+  }
+
+  DELETE_tarefa(tarefaASerRemovida: Tarefa) {
+    var indice = this.arrayDeTarefas.indexOf(tarefaASerRemovida);
+    var id = this.arrayDeTarefas[indice]._id;
+    this.service.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(
+    resultado => {
+      console.log(resultado); 
+      this.READ_tarefas(); 
+    });
+  }
+
+  UPDATE_tarefa(tarefaAserModificada: Tarefa) {
+    var indice = this.arrayDeTarefas.indexOf(tarefaAserModificada);
+    var id = this.arrayDeTarefas[indice]._id;
+    this.service.patch<Tarefa>(`${this.apiURL}/api/update/${id}`,
+    tarefaAserModificada).subscribe(
+    resultado => { console.log(resultado); this.READ_tarefas(); });
+   }
+}
